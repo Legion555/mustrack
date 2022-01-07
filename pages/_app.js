14129bb7ff776c0d5de1d80a3c83e806b5234748
legion555 <legioncommander555@gmail.com>
2022-01-08 00:08:36 +0700

    init

---
+++ pages/_app.js
@@ -0,0 +1,20 @@
+import '../styles/globals.css'
+//redux
+import {Provider} from 'react-redux'
+import {PersistGate} from 'redux-persist/integration/react';
+import {persistStore} from 'redux-persist';
+import store from '../store'
+
+let persistor = persistStore(store);
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Provider>
+      <PersistGate loading={null} persistor={persistor}>
+        <Component {...pageProps} />
+      </PersistGate>
+    </Provider>     
+  )
+}
+
+export default MyApp
---
+++ store/index.js
@@ -0,0 +1,24 @@
+import {configureStore} from '@reduxjs/toolkit'
+import { combineReducers } from 'redux'
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import userDataReducer, { userData } from '../slices/userDataSlice'
+
+const reducers = combineReducers({
+    userData: userDataReducer
+})
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: []
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware
+})
+
+export default store
